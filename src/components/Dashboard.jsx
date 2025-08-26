bb7e7636658d95cddc36630ddf5fe74f201b9b5f
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-08-26 16:19:31 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/dashboard');
+        const response = await axios.get('https://api-backend-0bi4.onrender.com/api/dashboard');
         setSearches(response.data);
       } catch (err) {
         setError('Failed to load data');
